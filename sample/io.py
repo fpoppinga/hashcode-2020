5560f73abe63f2f1cc06366b1e04ad13fc7e8280
Georg Felbinger <georg.felbinger@freiheit.com>
2020-02-19 19:17:07 +0100

    naiv feddich

--- sample/io.py
+++ sample/io.py
@@ -49,6 +49,7 @@ def score(problem: Problem, solution: Solution):
     for type in solution.types:
         score += problem.pizzas.types[type]
     assert score <= problem.pizzas.max_pizza
+    print("Score: {}".format(score))
     return score
 
 
