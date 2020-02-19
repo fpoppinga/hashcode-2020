2fa120e62b11853c84f25fbdcdad02e9d551fd38
Georg Felbinger <georg.felbinger@freiheit.com>
2020-02-19 19:25:44 +0100

    think like a millioniar

--- sample/naive.py
+++ sample/naive.py
@@ -2,13 +2,20 @@ import argparse
 
 import numpy as np
 from sample.io import Problem, Pizzas, Solution, score
+import os
+import zipfile
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
 
 
 def solve(problem):
     pizzas = []
     current = 0
     for id, type in reversed(list(enumerate(problem.pizzas.types))):
-        print("{}: {}".format(id, type))
         if (current + type) <= problem.pizzas.max_pizza:
             current += type
             pizzas.append(id)
@@ -19,12 +26,14 @@ def solve(problem):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--problem", default="in/a_example.in")
-    parser.add_argument("--solution", default="out/a_example.out")
-    args = parser.parse_args()
 
-    p = Problem(args.problem)
-    s = solve(p)
-    s.to_file(args.solution)
-    score(p, s)
+    for file in os.listdir("in"):
+        p = Problem("in/{}".format(file))
+        s = solve(p)
+        s.to_file("out/{}".format(file.replace(".in", ".out")))
+        score(p, s)
+
+    zipf = zipfile.ZipFile('../submit.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipdir('.', zipf)
+    zipf.close()
+
